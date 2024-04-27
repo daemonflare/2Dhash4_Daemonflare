9d2e6ba46a03ae5eee0857b31bbe7af94a4ee149
daemonflare <arber.isufi@city.ac.uk>
2024-04-27 18:42:04 +0100

    fixed store in tempnode

--- src/TemporaryNode.java
+++ src/TemporaryNode.java
@@ -63,7 +63,7 @@ public class TemporaryNode implements TemporaryNodeInterface {
             int storeKeyNo = key.split("\n").length;
             int storeValueNo = key.split("\n").length;
             writer.write("PUT? " + storeKeyNo + storeValueNo + "\n");
-            writer.write(key);
+            writer.write(key + "\n");
             writer.write(value);
             writer.flush();
 
