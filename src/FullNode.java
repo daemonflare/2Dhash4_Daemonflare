84aa255f7289311889c0aa6b2b60b87892b3a27a
daemonflare <arber.isufi@city.ac.uk>
2024-04-27 14:20:31 +0100

    FIXED NOTIFY (results may vary)

--- src/FullNode.java
+++ src/FullNode.java
@@ -97,7 +97,6 @@ public class FullNode implements FullNodeInterface {
                 writer.flush();
                 System.out.println("OHCE fired");
             } else if (request.startsWith("NOTIFY?")) {
-                System.out.println("REACHES HERE :)))))))))))");
                 try {
                     writer.write("NOTIFIED\n");
                     writer.flush();
