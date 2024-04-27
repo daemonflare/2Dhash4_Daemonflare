ec9bc29b3fcfe0b80c2ab9bd6ebce309ee03925a
daemonflare <arber.isufi@city.ac.uk>
2024-04-27 13:52:31 +0100

    fixes to tempnodeput? idk lol

--- src/TemporaryNode.java
+++ src/TemporaryNode.java
@@ -60,41 +60,61 @@ public class TemporaryNode implements TemporaryNodeInterface {
 
     public boolean store(String key, String value) {
         try {
-            writer.write("PUT? 1 " + value.split("\n").length + "\n");
-            writer.write(key + "\n");
-            writer.write(value + "\n");
+            int storeKeyNo = key.split("\n").length;
+            int storeValueNo = key.split("\n").length;
+            writer.write("PUT? " + storeKeyNo + storeValueNo + "\n");
+            writer.write(key);
+            writer.write(value);
             writer.flush();
 
-            String response = reader.readLine();
+            System.out.println("Sent: PUT? " + key + value);
 
-            String hexString = stringToHex(key);
-            /*List<FullNode.NodeData> nodes = getNearestNodes(reader,writer,hexString);
-            for (FullNode.NodeData ignored : nodes) {
-                //invokeNearest(hexString);
-            }*/
-            if (response.equals("SUCCESS")) {
-                return true;
-            } else if (response.equals("FAILED")) {
-                return false;
-            } else {
-                System.out.println("invalid: " + response);
-                return false;
+            String response = reader.readLine();
+            System.out.println(response);
+
+            if (response.startsWith("SUCCESS")) {
+                System.out.println("Store successful!");
+            } else if (response.startsWith("FAILED")) {
+                System.out.println("No bueno :(");
+                String hexID = stringToHex(key);
+                System.out.println("Hex ID to check is " + hexID);
+                writer.write("NEAREST? " + hexID + "\n");
+                writer.flush();
+                //TODO
+                response = reader.readLine();
+                if (response.startsWith("NODES")) {
+                    int numIter = Integer.parseInt(response.split(" ")[1]);
+                    String[] names = new String[numIter];
+                    String[] addrs = new String[numIter];
+                    for (int i = 0; i < numIter; i++) {
+                        // Reading all the node names and node addresses
+                        response = reader.readLine();
+                        System.out.println(response);
+                        names[i] = response;
+
+                        response = reader.readLine();
+                        System.out.println(response);
+                        addrs[i] = response;
+                    }
+                    // i think, after this, the 3 closest nodes will be shown
+
+                    System.out.println("THESE THREE NODES ARE CLOSER!");
+                }
             }
         } catch (IOException e) {
-            e.printStackTrace();
-            return false;
+            throw new RuntimeException(e);
         }
+        return false;
     }
 
     @Override
     public String get(String key) {
         try {
-            if (!key.endsWith("\n")){
+            // if it doesn't end in newline, add one
+            if (!key.endsWith("\n")) {
                 key += "\n";
             }
-
             int keyNo = key.split("\n").length;
-
             writer.write("GET? " + keyNo + "\n");
             writer.write(key);
             writer.flush();
@@ -109,9 +129,9 @@ public class TemporaryNode implements TemporaryNodeInterface {
                 int valueNo = Integer.parseInt(response.split(" ")[1]);
                 StringBuilder value = new StringBuilder();
 
-                for (int i = 0; i < valueNo; i++){
+                for (int i = 0; i < valueNo; i++) {
                     value.append(reader.readLine());
-                    if (i < valueNo - 1){
+                    if (i < valueNo - 1) {
                         value.append("\n");
                     }
                 }
@@ -127,12 +147,11 @@ public class TemporaryNode implements TemporaryNodeInterface {
                 writer.flush();
 
                 response = reader.readLine();
-                if (response.startsWith("NODES")){
+                if (response.startsWith("NODES")) {
                     int numIter = Integer.parseInt(response.split(" ")[1]);
                     String[] names = new String[numIter];
                     String[] addrs = new String[numIter];
-                    for (int i = 0; i < numIter; i++){
-                        // Reading all the node names and node addresses
+                    for (int i = 0; i < numIter; i++) {
                         response = reader.readLine();
                         System.out.println(response);
                         names[i] = response;
@@ -142,24 +161,20 @@ public class TemporaryNode implements TemporaryNodeInterface {
                         addrs[i] = response;
                     }
 
-                    //Check if the node has been visited
-                    for (int i = 0; i < numIter; i++){
-                        System.out.println("REACHES SECOND FOR LOOP");
-                        if(visitedNodes.contains(names[i])){
+                    for (int i = 0; i < numIter; i++) {
+                        if (visitedNodes.contains(names[i])) {
                             return null;
-                        }else{
-                            // Otherwise, add to the list
+                        } else {
                             visitedNodes.add(names[i]);
                         }
 
-                        if(this.start(names[i], addrs[i])){
+                        if (this.start(names[i], addrs[i])) {
                             String value = get(key);
-                            if (value != null){
+                            if (value != null) {
                                 return value;
                             }
                         }
                     }
-
                 }
                 return null;
             }
@@ -188,7 +203,8 @@ public class TemporaryNode implements TemporaryNodeInterface {
             return null;
         }
     }
-    public void terminateConnection(String reason) { // hook, line and sinker >:)
+
+    public void terminateConnection(String reason) { // DESTROY THEM ALL
         try {
             System.out.println("REACHES HERE :)");
             writer.write("END " + reason);
